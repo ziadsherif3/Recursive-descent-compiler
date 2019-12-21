5d6145351d4f151990366bc695fac39c9c60d21b
ziadsherif3 <ziadsherif3@gmail.com>
2019-12-21 03:27:00 +0200

    Added procedures for each non-terminal

--- src/app/RD_compiler.java
+++ src/app/RD_compiler.java
@@ -49,26 +49,106 @@ public class RD_compiler {
 
     public static void A(){
 
+        if(V() == 0){
+            System.out.print("FAIL");
+            System.exit(0);
+        }
+        CHECK('=');
+        E();
+        System.out.print("STORE ");
+
     }
 
     public static void E(){
-        
+
+        if(LA == '-'){
+            SCAN();
+            T();
+            System.out.print("NEG ");
+        }
+        else{
+            T();
+        }
+
+        while(LA == '+' || LA == '-'){
+            if(LA == '+'){
+                SCAN();
+                T();
+                System.out.print("ADD ");
+            }
+            else{
+                SCAN();
+                T();
+                System.out.print("SUB ");
+            }
+        }
+
     }
 
     public static void T(){
         
+        F();
+
+        while(LA == '*' || LA == '/'){
+            if(LA == '*'){
+                SCAN();
+                F();
+                System.out.print("MUL ");
+            }
+            else{
+                SCAN();
+                F();
+                System.out.print("DIV ");
+            }
+        }
+
     }
 
     public static void F(){
         
+        if(LA == '('){
+            SCAN();
+            E();
+            CHECK(')');
+        }
+        else{
+            if(C() == 0){
+                if(V() == 1){
+                    System.out.print("LOAD ");
+                }
+                else{
+                    System.out.print("FAIL");
+                    System.exit(0);
+                }
+            }
+        }
+
     }
 
-    public static void C(){
+    public static int C(){
         
+        if(LA >= '0' && LA <= '9'){
+            System.out.print("LIT " + LA + " ");
+            SCAN();
+            return 1;
+        }
+        else{
+            return 0;
+        }
+
     }
 
-    public static void V(){
-        
+    public static int V(){
+
+        if(LA >= 'A' && LA <= 'Z'){
+            System.out.print("LIT " + LA + " ");
+            SCAN();
+            return 1;
+        }
+        else{
+            return 0;
+        }
+
     }
 
 }
